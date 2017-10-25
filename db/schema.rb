870242290988fe02bbffcebbf19a898039885362
Jason Rovner <jason.rovner@gmail.com>
2017-10-25 19:01:40 +0000

    Add comment model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025164500) do
+ActiveRecord::Schema.define(version: 20171025185216) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "user_id"
+    t.integer  "recipe_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "grains", force: :cascade do |t|
     t.string "name"
